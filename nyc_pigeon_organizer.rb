3423c2168c007dd2bb6287e7c97956023513ca97
Elizabeth Jacobs <elizabethjjacobs@gmail.com>
2016-11-15 18:55:28 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  org_hash = {}
+  data.each do |attribute, value_hash|
+    value_hash.each do |options, options_array|
+      options_array.each do |name|
+          org_hash[name] ||= {}
+          org_hash[name][attribute] ||= []
+          org_hash[name][attribute] << options.to_s
+      end
+    end
+  end
+  org_hash  
 end
